a16844e46f0b590a5fe521d3a1f21c1dc76db81a
pierre0210 <gamelauncher0210@gmail.com>
2023-01-06 23:41:50 +0800

    feat: add username & password input

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,26 @@
 <script setup>
 import NavBar from "../components/NavBar.vue";
+
+function submitLogin() {}
 </script>
 
 <template>
   <NavBar />
+  <form @submit="submitLogin" class="p-2">
+    <label>User name: </label>
+    <input
+      type="text"
+      v-model="username"
+      class="border-black border-2 rounded my-1"
+    />
+    <label class="ml-1">Password: </label>
+    <input
+      type="password"
+      v-model="password"
+      class="border-black border-2 rounded my-1"
+    />
+    <button type="submit" class="ml-1 px-1 rounded border-black border-2">
+      Login
+    </button>
+  </form>
 </template>
