967a7ef86474cc0a25f12cad8f0e37cccf4520b2
pierre0210 <gamelauncher0210@gmail.com>
2023-01-07 22:15:43 +0800

    feat: Sign up page

---
+++ src/views/SignupView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import NavBar from "../components/NavBar.vue";
+
+const submitRegistration = () => {};
+</script>
+
+<template>
+  <NavBar />
+  <form @submit="submitRegistration" class="p-2">
+    <label>User name: </label>
+    <input
+      type="text"
+      v-model="username"
+      class="border-black border-2 rounded my-1"
+    />
+    <label class="ml-1">Password: </label>
+    <input
+      type="password"
+      v-model="password"
+      class="border-black border-2 rounded my-1"
+    />
+    <button type="submit" class="ml-1 px-1 rounded border-black border-2">
+      Sign up
+    </button>
+  </form>
+</template>
